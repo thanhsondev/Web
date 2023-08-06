0399f8fe0bf689714cd4253acfa5426bbb230559
thanhson-itk6 <duongvanthanhson5@gmail.com>
2023-08-07 02:42:34 +0700

    first commit

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,10 +1,34 @@
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Web.Data;
+using Web.Models;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebContext") ?? throw new InvalidOperationException("Connection string 'WebContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        //Initialize Roles and Users. This one will create a user with an Admin-role aswell as a separate User-role. See UserRoleInitializer.cs in Models
+        //UserRoleInitializer.InitializeAsync(services).Wait();
+        // Seed book data
+        SeedData.Initialize(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occured while attempting to seed the database");
+    }
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
